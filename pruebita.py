c0095551a2d3ec47871c7b48124e7d1ef634365a
msebastianvg <109251380+msebastianvg@users.noreply.github.com>
2023-10-08 18:35:18 -0300

    Update pruebita.py

--- pruebita.py
+++ pruebita.py
@@ -299,18 +299,9 @@ print(f'Desviación Estándar de SINGLE: {desviacion_estandar:.2f}')
 
 
 st.title("Monto personal:")
-col1, col2 = st.beta_columns([1, 4])
-
-# Campo de entrada de texto en la segunda columna
-with col2:
-    input_text = st.text_input("Ingresa una palabra:")
-
-# Verificar si la palabra ingresada es 'lokura'
+input_text = st.text_input("Ingresa tu palabra clave:")
 if input_text.lower() == 'lokura':
     resultado = 1
 else:
     resultado = 0
-
-# Mostrar el resultado en un label en la segunda columna
-with col2:
-    st.write(f"Resultado: {resultado}")
+st.write(f"Resultado: {resultado}")
