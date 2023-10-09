6360f34706e7d5be6c09f31728ebd3e1a53e39dd
msebastianvg <109251380+msebastianvg@users.noreply.github.com>
2023-10-08 21:37:35 -0300

    Update pruebita.py

--- pruebita.py
+++ pruebita.py
@@ -58,10 +58,6 @@ st.write(fig)
 df['DATE'] = pd.to_datetime(df['DATE'])
 apuestas_ganadas = df[df['WL'] == 1]
 ganancias_por_dia = apuestas_ganadas.groupby('DATE')['WL'].count()
-st.write(
-    f"<style>.chart-single-bar rect {{ fill: green; }}</style>", 
-    unsafe_allow_html=True
-)
 st.write("Cantidad de Apuestas Ganadas por Día")
 st.bar_chart(ganancias_por_dia)
 
@@ -119,65 +115,6 @@ st.pyplot(fig)
 
 
 
-# df = pd.read_excel('E:\\B\\bets.xlsx')
-
-# Calcular el valor mínimo de la columna 'SINGLE'
-min_single = df['PERCENTAGE'].min()
-
-# Calcular el valor máximo de la columna 'SINGLE' y agregar 1000
-max_single = df['PERCENTAGE'].max() + 0.01
-
-# Convertir la columna 'date' a tipo datetime
-df['DATE'] = pd.to_datetime(df['DATE'])
-
-# Seleccionar el último registro de cada fecha
-df = df.groupby('DATE').tail(1)
-
-# Calcular el promedio global de 'SINGLE'
-average_single_global = df['PERCENTAGE'].mean()
-
-# Crear un gráfico de barras que muestra la variación de 'SINGLE' con respecto a las fechas
-st.write("Variación de 'SINGLE' con respecto a las Fechas")
-
-fig, ax = plt.subplots(figsize=(10, 6))
-
-# Asignar colores según 'WL' en el último registro de cada fecha
-colors = ['lightgreen' if wl == 1 else 'pink' for wl in df['WL']]
-
-ax.bar(df['DATE'], df['PERCENTAGE'], color=colors)
-
-# Personalizar el gráfico
-ax.set_xlabel('FECHA')
-ax.set_ylabel('POZO TOTAL')
-ax.set_title('DETALLE DEL POZO')
-ax.set_ylim([min_single, max_single])  # Establecer el límite superior del eje Y
-ax.yaxis.set_major_formatter('{:.0%}'.format)
-
-# Rotar las etiquetas del eje x para una mejor legibilidad
-plt.xticks(rotation=45)
-
-# Ajustar una curva cuadrática (segundo grado) a los datos
-x = np.arange(len(df['DATE']))
-y = df['PERCENTAGE'].values
-curve_fit = np.polyfit(x, y, 2)
-quadratic_curve = np.poly1d(curve_fit)
-
-# Calcular valores de la curva cuadrática
-quadratic_values = quadratic_curve(x)
-
-# Agregar la línea que muestra el promedio global de 'SINGLE'
-ax.axhline(average_single_global, color='blue', linestyle='--', label='PROMEDIO que no dice nada')
-
-# Agregar la curva cuadrática al gráfico
-ax.plot(df['DATE'], quadratic_values, label='CURVA que tengo que recalcular', color='purple')
-
-# Mostrar la leyenda
-ax.legend()
-
-# Mostrar el gráfico en Streamlit
-st.pyplot(fig)
-
-
 
 # df = pd.read_excel('E:\\B\\bets.xlsx')
 
@@ -211,84 +148,7 @@ print(equation)
 
 
 
-# df = pd.read_excel('E:\\B\\bets.xlsx')
 
-reference_date = datetime(2023, 1, 1)  # Establece tu fecha de referencia
-df['DAYS_FROM_REFERENCE'] = (df['DATE'] - reference_date).dt.days
-
-# Extrae los datos de 'DAYS_FROM_REFERENCE' y 'SINGLE' del DataFrame
-X = df['DAYS_FROM_REFERENCE'].values
-Y = df['PERCENTAGE'].values
-
-# Calcula la media de X e Y
-mean_X = np.mean(X)
-mean_Y = np.mean(Y)
-
-# Calcula la desviación estándar de X e Y
-std_X = np.std(X)
-std_Y = np.std(Y)
-
-# Calcula el coeficiente de la pendiente (a) y el intercepto (b) de la regresión lineal
-a = np.sum((X - mean_X) * (Y - mean_Y)) / np.sum((X - mean_X) ** 2)
-b = mean_Y - a * mean_X
-
-# Construye la ecuación lineal Y = aX + b
-equation = f"Y = {a:.2f}X + {b:.2f}"
-
-# Imprime la ecuación lineal
-print("Ecuación Lineal:")
-print(equation)
-
-
-a = 554.83
-b = 518903.93
-
-df['DATE'] = pd.to_datetime(df['DATE'])
-
-# Ordena el DataFrame por la columna 'DATE'
-df.sort_values(by='DATE', inplace=True)
-
-# Extrae los datos de 'DATE' y 'SINGLE' del DataFrame
-X = df['DATE'].values
-Y = df['PERCENTAGE'].values
-
-# Convierte las fechas en días transcurridos desde una fecha de referencia
-reference_date = X[0]  # Establece la fecha de referencia como la primera fecha en tus datos
-X = (X - reference_date).astype('timedelta64[D]').astype(int)
-
-# Cálculo de la función lineal
-def linear_regression(x):
-    a = 0.001
-    b = -0.007
-    #b = 518903.93
-    #a = (Y[-1] - Y[0]) / (X[-1] - X[0])  # Calcula la pendiente 'a' usando el primer y último punto
-    #b = Y[0] - a * X[0]  # Calcula el término de intercepción 'b'
-    return a * x + b
-
-# Crea una gráfica
-fig, ax = plt.subplots()
-ax.plot(X, Y, label='GANANCIA (%)', linestyle='-', marker='o')
-ax.plot(X, linear_regression(X), label='REGRESIÓN LINEAL', linestyle='--', color='red')
-ax.set_xlabel('FECHA')
-ax.set_ylabel('GANANCIA (%)')
-ax.set_title('REGRESIÓN LINEAL que está de locos')
-plt.xticks(rotation=45)  # Rotación de las etiquetas del eje X para mejor visualización
-plt.legend()
-ax.yaxis.set_major_formatter('{:.0%}'.format)
-
-# Muestra la gráfica en Streamlit
-st.pyplot(fig)
-
-media_single = df['PERCENTAGE'].mean()
-
-# Calcula el error medio absoluto (MAE)
-mae = (df['PERCENTAGE'] - media_single).abs().mean()
-
-print(f'Error Medio Absoluto (MAE): {mae:.2f}')
-
-desviacion_estandar = df['PERCENTAGE'].std()
-
-print(f'Desviación Estándar de SINGLE: {desviacion_estandar:.2f}')
 
 
 st.subheader('Monto personal')
