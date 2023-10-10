36d056e150c2e63270fe498840f94070083f3595
msebastianvg <109251380+msebastianvg@users.noreply.github.com>
2023-10-09 21:24:36 -0300

    Update pruebita.py

--- pruebita.py
+++ pruebita.py
@@ -53,7 +53,7 @@ fig = px.bar(
     labels={"x": "", "y": ""},
 #    title="Apuestas Ganadas vs. Apuestas Perdidas",
     color_discrete_map={"Ganadas": "lightgreen", "Perdidas": "mistyrose"},
-    showlegend=False,
+    showlegend=False
 )
 st.write(fig)
 
