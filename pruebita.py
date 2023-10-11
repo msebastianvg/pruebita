0f924d44951b4290435a830a870804df47cf4a98
msebastianvg <109251380+msebastianvg@users.noreply.github.com>
2023-10-11 16:01:45 -0300

    Update pruebita.py

--- pruebita.py
+++ pruebita.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 from datetime import datetime
 import plotly.express as px
+import plotly.graph_objects as go
 # import seaborn as sns
 # from sklearn.linear_model import LinearRegression
 
@@ -77,7 +78,26 @@ fig.update_layout(
 st.plotly_chart(fig)
 
 
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
+fig = go.Figure()
+fig.add_trace(go.Bar(
+    x=months,
+    y=[20, 14, 25, 16, 18, 22, 19, 15, 12, 16, 14, 17],
+    name='Primary Product',
+    marker_color='indianred'
+))
+fig.add_trace(go.Bar(
+    x=months,
+    y=[19, 14, 22, 14, 16, 19, 15, 14, 10, 12, 12, 16],
+    name='Secondary Product',
+    marker_color='lightsalmon'
+))
+
+# Here we modify the tickangle of the xaxis, resulting in rotated labels.
+fig.update_layout(barmode='group', xaxis_tickangle=-45)
+fig.show()
 
 
 total_wins = (df['WL'] == 1).sum()
