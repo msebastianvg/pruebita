41141bcd8aecea17cd7c685e185d0ec3a6891aed
msebastianvg <109251380+msebastianvg@users.noreply.github.com>
2023-11-13 18:34:55 -0300

    Update pruebita.py

--- pruebita.py
+++ pruebita.py
@@ -98,7 +98,7 @@ df_cleaned = df.dropna(subset=['PERCENTAGE'])
 
 if not df_cleaned.empty:
     df_cleaned = df_cleaned.iloc[::-1] 
-    ultimo_percentage = df_cleaned['PERCENTAGE'][df_cleaned['PERCENTAGE'] > 0].iloc[-1]
+    ultimo_percentage = df_cleaned['PERCENTAGE'][df_cleaned['PERCENTAGE'] > -10].iloc[-1]
 else:
     ultimo_percentage = None 
 
